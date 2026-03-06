df0e91cbab779a6d34b8617f9120379ed7ac174a
federiconbaez <federiconbaez@gmail.com>
2026-03-06 02:49:08 -0300

    feat(modals): Implementé modales reutilizables y menú de usuario actualizado

---
+++ client/src/acdm/components/system/modals/index.js
@@ -0,0 +1,6 @@
+export { AlumnoModal } from './AlumnoModal.jsx';
+export { DocenteModal } from './DocenteModal.jsx';
+export { EscuelaModal } from './EscuelaModal.jsx';
+export { InformeModal } from './InformeModal.jsx';
+export { ProyectoModal } from './ProyectoModal.jsx';
+export { VisitaModal } from './VisitaModal.jsx';
